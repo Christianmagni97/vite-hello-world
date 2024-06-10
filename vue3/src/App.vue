930f156c6866a573e0b221412e27e0687f57a6ea
Christianmagni97 <christianmagni28@gmail.com>
2024-06-10 23:22:06 +0200

    importato AppTitle in html e vue

---
+++ vue3/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppTitle from './components/AppTitle.vue'
+components: {
+  AppTitle
+}
+</script>
+
+<template>
+</template>
+
+<style scoped>
+</style>
---
+++ vue3/src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+<template>
+    <header>
+        <h1 class="header_title">
+            La mia prima app con vite!
+        </h1>
+    </header>
+</template>
+<style>
+
+</style>
